fb8c601fc8a6fd10c5cc9a105f150cd622a20459
Brent <puddletowndesign@gmail.com>
2017-04-22 22:27:18 -0500

    updated biniaries

--- backup-configs.sh
+++ backup-configs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+echo "backing up bin"
 cd ~/Documents/Dev/bin
 git add .
 git commit -m "updated biniaries"
 git push origin master
 
+echo "backing up brew"
 cd ~/Documents/Dev/Brew
 brew list > packages.list
 brew cask list > casks-all.list
@@ -12,26 +14,31 @@ git add .
 git commit -m "updated brew packages"
 git push origin mac
 
+echo "backing up git"
 cd ~/Documents/Dev/Git/
 git add .
 git commit -m "updated git preferences"
 git push origin mac
 
+echo "backing up installer"
 cd ~/Documents/Dev/Installer
 git add .
 git commit -m "updated installer preferences"
 git push origin mac
 
+echo "backing up iterm"
 cd ~/Documents/Dev/iTerm
 git add .
 git commit -m "updated iterm preferences"
 git push origin mac
 
+echo "backing up zsh"
 cd ~/Documents/Dev/ZSH
 git add .
 git commit -m "updated zsh preferences"
 git push origin mac
 
+echo "backing up launchd"
 cd ~/Documents/Dev/LaunchAgents
 git add com.puddletowndesign.backup.plist com.puddletowndesign.update.plist README.md
 git commit -m "updated jobs"
